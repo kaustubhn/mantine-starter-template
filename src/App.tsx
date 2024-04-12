4d0cc09fec1f09c816bf9cf314c59f2d3b9be7ad
kaustubhn <kaustubhnarkhede88@gmail.com>
2024-04-12 11:06:14 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <MantineProvider>
+        Hello from Mantine Starter Template!
+      </MantineProvider>
+    </>
+  )
+}
+
+export default App
