4e649f5d24a06667adf8af7daf9193ac8a357dd6
kaustubhn <kaustubhnarkhede88@gmail.com>
2024-04-12 11:33:45 +0530

    Add navigation notifications, date, & modals to the template

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import '@mantine/dates/styles.css';
+import '@mantine/nprogress/styles.css';
 import './App.css'
+import { MantineProvider } from '@mantine/core';
+import { DatesProvider } from '@mantine/dates';
+import { Notifications } from '@mantine/notifications';
+import { NavigationProgress } from '@mantine/nprogress';
+import { ModalsProvider } from '@mantine/modals';
 
 function App() {
 
   return (
     <>
       <MantineProvider>
-        Hello from Mantine Starter Template!
+        <NavigationProgress />
+        <Notifications />
+        <DatesProvider settings={{ locale: 'en', firstDayOfWeek: 0, weekendDays: [0], timezone: 'UTC' }}>
+          <ModalsProvider>
+            Hello from Mantine Starter Template!
+          </ModalsProvider>
+        </DatesProvider>
       </MantineProvider>
     </>
   )
